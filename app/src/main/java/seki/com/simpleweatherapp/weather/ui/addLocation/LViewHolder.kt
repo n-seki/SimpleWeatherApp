b756bf58f6f17c5aaf2e72ae0590668fa96b8274
n-seki <developer.n.seki@gmail.com>
2018-06-03 15:57:05 +0900

    delete unused class

--- app/src/main/java/seki/com/simpleweatherapp/weather/ui/addLocation/LViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package seki.com.simpleweatherapp.weather.ui.addLocation
-
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import seki.com.simpleweatherapp.databinding.LayoutLocationListItemBinding
-
-class LViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    val binder by lazy { LayoutLocationListItemBinding.bind(itemView) }
-}
