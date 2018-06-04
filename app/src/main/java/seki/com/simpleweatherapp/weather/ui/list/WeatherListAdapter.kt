f1350505136a8ca0be318c07712ba59500fded27
n-seki <developer.n.seki@gmail.com>
2018-06-04 22:20:36 +0900

    remove unused import

--- app/src/main/java/seki/com/simpleweatherapp/weather/ui/list/WeatherListAdapter.kt
+++ app/src/main/java/seki/com/simpleweatherapp/weather/ui/list/WeatherListAdapter.kt
@@ -1,6 +1,5 @@
 package seki.com.simpleweatherapp.weather.ui.list
 
-import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
